317b61c2e94828c9055e98c4ca0aad7a9245e937
Marcus Eklund <marckus@gmail.com>
2022-07-31 19:17:11 +0200

    Seperated connectionstring from AppSettings + Enabled PostgreSQL

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,9 +1,22 @@
+using Microsoft.EntityFrameworkCore;
 using CommandAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var databasebuilder = new Npgsql.NpgsqlConnectionStringBuilder();
+databasebuilder.ConnectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection");
+databasebuilder.Username = builder.Configuration["UserID"];
+databasebuilder.Password = builder.Configuration["Password"];
+
 builder.Services.AddControllers();
-builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(databasebuilder.ConnectionString));
 
 var app = builder.Build();
+
+if (builder.Environment.IsDevelopment()){
+  app.UseDeveloperExceptionPage();
+}
+
 app.MapControllers();
 app.Run();
